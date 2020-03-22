0b3fde79213de3909173f2a7e17c8c94fe91b91f
Adam Fields <adam.elliot.fields@gmail.com>
2020-03-22 16:20:02 -0400

    chore(jest): Run test without `react-scripts` wrapper.

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+const createJestConfig = require('react-scripts/scripts/utils/createJestConfig');
+const path = require('path');
+
+const jestConfig = createJestConfig(relativePath =>
+  path.join(__dirname, 'node_modules', 'react-scripts', relativePath),
+);
+
+module.exports = {
+  ...jestConfig,
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/stories/**/*.{js,jsx,ts,tsx}',
+    '!src/index.ts',
+    '!src/setupTests.js',
+  ],
+};
